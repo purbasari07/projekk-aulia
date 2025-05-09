c21a37aed833b6d98c70aec2e3ab0f8d0e25197b
purbasari07 <aulianitipurbasari99@gmail.com>
2025-05-10 00:10:31 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,67 @@
+let books = [
+  {
+    "id": 1746600709489,
+    "title": "TIARA DI PABRIK SARDEN",
+    "author": "Joko Ngawur "
+  },
+  {
+    "id": 1746623067449,
+    "title": "Bumi Manusia",
+    "author": "Pramoedya Ananta"
+  },
+  {
+    "id": 1746623157219,
+    "title": "Teras",
+    "author": "Henry Manampiring"
+  },
+  {
+    "id": 1746623219103,
+    "title": "Laut Bercerita",
+    "author": "Laila Salikha"
+  },
+  {
+    "id": 1746623597613,
+    "title": "Sang Pemimpi",
+    "author": "Andrea"
+  },
+  {
+    "id": 1746666782692,
+    "title": "PENGEPUNGAN DEWI PERSIK DI BUKIT AMBON",
+    "author": "Nona Ambon Pica"
+  },
+  {
+    "id": 1746667031694,
+    "title": "Jalan Pulang",
+    "author": "Tiara Saraswati Ahmandhita"
+  },
+  {
+    "id": 1746686265428,
+    "title": "Senja",
+    "author": "Indilla R"
+  },
+  {
+    "id": 1746704236646,
+    "title": "Harian Laila",
+    "author": "Clarissha Anantha"
+  },
+  {
+    "id": 1746748769909,
+    "title": "Sang Penghibur",
+    "author": "Talita Viky Sakhi"
+  },
+  {
+    "id": 1746752858662,
+    "title": "Mindset",
+    "author": "Carol S. Dweck"
+  },
+  {
+    "id": 1746754273432,
+    "title": "Bumi Di Langit",
+    "author": "Jilan Indah Aprilianti"
+  },
+  {
+    "id": 1746758290195,
+    "title": "Laskar Pelangi",
+    "author": "Maman Hermawan"
+  }
+];module.exports = { books };
